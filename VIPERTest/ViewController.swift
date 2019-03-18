8e0ce760a97b515b77af3bcdad0d0c7cd98ebbcf
Olga Vorona <olgavorona1@gmail.com>
2019-03-18 12:29:30 +0300

    Initial Commit

---
+++ VIPERTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPERTest
+//
+//  Created by Olga Vorona on 18/03/2019.
+//  Copyright © 2019 Olga Vorona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
